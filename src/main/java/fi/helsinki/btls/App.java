d19ad7a7c26df7f0ac783ac50ca7d698869467c0
jhoneagle <eaglekjohn@gmail.com>
2019-05-17 15:34:40 +0300

    gson added and json models made

--- src/main/java/fi/helsinki/btls/App.java
+++ src/main/java/fi/helsinki/btls/App.java
@@ -3,13 +3,29 @@
  */
 package fi.helsinki.btls;
 
+import com.google.gson.Gson;
+import fi.helsinki.btls.domain.LocationModel;
+import fi.helsinki.btls.domain.ObservationModel;
 import fi.helsinki.btls.io.ConnectionListener;
 import fi.helsinki.ubimqtt.UbiMqtt;
 
 public class App {
     public static void main(String[] args) throws Exception {
+        String json = "{ 'raspId':'f8fe6w739fweuy', 'beaconId':'s6383f47f364', 'volume':129 }";
+        Gson gson = new Gson();
+        ObservationModel user = gson.fromJson(json, ObservationModel.class);
+
+        System.out.println("Observation json to model:");
+        System.out.println("json: " + json);
+        System.out.println(user);
+
+        System.out.println();
+        System.out.println("LocationModel to json:");
+        LocationModel model = new LocationModel("7fa98fyr97hg3h983f34hfu", 200, 200, 34, 21);
+        System.out.println(model);
+        /*
         UbiMqtt mqtt = new UbiMqtt("iot.ubikampus.net");
         ConnectionListener connectionListener = new ConnectionListener(mqtt, "ohtu/test");
-        mqtt.connect(connectionListener);
+        mqtt.connect(connectionListener);*/
     }
 }
---
+++ src/main/java/fi/helsinki/btls/domain/LocationModel.java
@@ -0,0 +1,65 @@
+package fi.helsinki.btls.domain;
+
+public class LocationModel {
+    private String beaconId;
+    private long x;
+    private long y;
+    private long xr;
+    private long yr;
+
+    public LocationModel() {
+    }
+
+    public LocationModel(String beaconId, long x, long y, long xr, long yr) {
+        this.beaconId = beaconId;
+        this.x = x;
+        this.y = y;
+        this.xr = xr;
+        this.yr = yr;
+    }
+
+    public String getBeaconId() {
+        return beaconId;
+    }
+
+    public void setBeaconId(String beaconId) {
+        this.beaconId = beaconId;
+    }
+
+    public long getX() {
+        return x;
+    }
+
+    public void setX(long x) {
+        this.x = x;
+    }
+
+    public long getY() {
+        return y;
+    }
+
+    public void setY(long y) {
+        this.y = y;
+    }
+
+    public long getXr() {
+        return xr;
+    }
+
+    public void setXr(long xr) {
+        this.xr = xr;
+    }
+
+    public long getYr() {
+        return yr;
+    }
+
+    public void setYr(long yr) {
+        this.yr = yr;
+    }
+
+    @Override
+    public String toString() {
+        return "{ 'beaconId':'" + beaconId + "', 'x':" + x + ", 'y':" + y + ", 'xr':" + xr + ", 'yr':" + yr + " }";
+    }
+}
