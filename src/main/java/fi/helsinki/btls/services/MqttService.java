518a47bb79cdb5945e3f915a4ef5cf4e203497e2
Emil Andersson <anderssone0@gmail.com>
2019-06-03 17:38:18 +0300

    Comment out test data

--- src/main/java/fi/helsinki/btls/services/MqttService.java
+++ src/main/java/fi/helsinki/btls/services/MqttService.java
@@ -41,15 +41,15 @@ public class MqttService implements IMqttService{
 
     @Override
     public List<ObservationModel> getObservations() {
-        List<ObservationModel> test = new ArrayList<ObservationModel>();
+        /*List<ObservationModel> test = new ArrayList<ObservationModel>();
         test.add(new ObservationModel("rasp1", "", 123.45));
         test.add(new ObservationModel("rasp2", "", 71.3));
         test.add(new ObservationModel("rasp3", "", 18.6));
         test.add(new ObservationModel("rasp4", "", 96.33));
         test.add(new ObservationModel("rasp2", "", 50.33));
 
-        return test;
-        //return observations;
+        return test;*/
+        return observations;
     }
 
     @Override
