46b77b0f8e0c2a11a9f7c3c31b0aea4be4289b3c
Emil Andersson <anderssone0@gmail.com>
2019-06-04 22:13:06 +0300

    Class for beacon

---
+++ src/main/java/fi/helsinki/btls/domain/Beacon.java
@@ -0,0 +1,36 @@
+package fi.helsinki.btls.domain;
+
+import java.util.List;
+
+/***
+ * Class representing one beacon
+ */
+public class Beacon {
+    private String id;
+    private int minVolume;
+    private List<ObservationModel> observations;
+
+    public Beacon(String id) {
+        this.id = id;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public int getMinVolume() {
+        return minVolume;
+    }
+
+    public void setMinVolume(int minVolume) {
+        this.minVolume = minVolume;
+    }
+
+    public List<ObservationModel> getObservations() {
+        return observations;
+    }
+
+    public void setObservations(List<ObservationModel> observations) {
+        this.observations = observations;
+    }
+}
