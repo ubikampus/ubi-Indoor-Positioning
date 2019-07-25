a583446f92f3d3414eefb9ed2aeff223a7d9ea6e
Emil Andersson <anderssone0@gmail.com>
2019-07-25 13:40:38 +0300

    First Kalman implementation

--- src/main/java/fi/helsinki/ubipositioning/trilateration/LocationService.java
+++ src/main/java/fi/helsinki/ubipositioning/trilateration/LocationService.java
@@ -1,5 +1,6 @@
 package fi.helsinki.ubipositioning.trilateration;
 
+import fi.helsinki.ubipositioning.utils.Kalman;
 import org.apache.commons.math3.fitting.leastsquares.LeastSquaresOptimizer;
 import org.apache.commons.math3.fitting.leastsquares.LevenbergMarquardtOptimizer;
 import java.util.*;
@@ -8,6 +9,8 @@ import com.lemmingapex.trilateration.TrilaterationFunction;
 import fi.helsinki.ubipositioning.datamodels.*;
 import fi.helsinki.ubipositioning.utils.IObserverService;
 import fi.helsinki.ubipositioning.utils.PathLossModel;
+
+import javax.print.attribute.HashAttributeSet;
 
 /**
  * Base logic for trilateration calculation that finds solution if possible in n-dimensional space.
@@ -42,18 +45,31 @@ abstract class LocationService implements ILocationService {
                                                 List<Observation> obs) {
         List<double[]> pos = new ArrayList<>();
         List<Double> dist = new ArrayList<>();
+        HashMap<String, List<Double>> measurements = new HashMap();
+        Kalman k = new Kalman();
 
         // Go through observations from newest to latest.
         for (int i = obs.size() - 1; i >= 0; i--) {
             Observation model = obs.get(i);
 
-            // preventing double value from observers.
-            if (!observersChecked.contains(model.getRaspId())) {
-                dist.add(pathLossModel.calculateDistance(model.getRssi(), measuredPower)); // changing rssi into millimeters distance.
-                pos.add(iObserverService.getObserver(model.getRaspId()).getPosition());
-                observersChecked.add(model.getRaspId());
+            if (!measurements.containsKey(model.getRaspId())) {
+                measurements.put(model.getRaspId(), new ArrayList<>());
             }
+            measurements.get(model.getRaspId()).add(model.getRssi());
+
+            // preventing double value from observers.
+            //if (!observersChecked.contains(model.getRaspId())) {
+            //    dist.add(pathLossModel.calculateDistance(model.getRssi(), measuredPower)); // changing rssi into millimeters distance.
+            //    pos.add(iObserverService.getObserver(model.getRaspId()).getPosition());
+            //    observersChecked.add(model.getRaspId());
+            //}
         }
+        for (Map.Entry<String, List<Double>> val :
+                measurements.entrySet()) {
+            dist.add(pathLossModel.calculateDistance(k.Calculate(val.getValue().toArray(new Double[0])), measuredPower)); // changing rssi into millimeters distance.
+            pos.add(iObserverService.getObserver(val.getKey()).getPosition());
+        }
+
 
         double[][] positions = new double[pos.size()][pos.get(0).length]; // Positions of observers (BLE listeners).
         double[] distances = new double[dist.size()]; // Distances from each observer to target BLE device.
